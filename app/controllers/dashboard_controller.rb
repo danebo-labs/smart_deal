a7c5667c77ac5d4ffd33d8a13b79b7bc8d3c05f3
Lahiri San <lahiri.sanchez@gmail.com>
2026-01-14 13:58:20 -0300

    refactor: extract metrics helpers to concern
    - Created MetricsHelper concern to eliminate duplication
    - Refactored HomeController to use concern
    - Refactored DashboardController to use concern
    - Eliminated ~46 lines of duplicated metrics code

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class DashboardController < ApplicationController
+  include MetricsHelper
+
   def index
     @current_metrics = current_metrics
     @monthly_totals = monthly_totals
@@ -25,29 +27,6 @@ class DashboardController < ApplicationController
 
   private
 
-  def current_metrics
-    today = Date.current
-    s3_size_bytes = CostMetric.find_by(date: today, metric_type: :s3_total_size)&.value || 0
-
-    {
-      today_tokens: CostMetric.find_by(date: today, metric_type: :daily_tokens)&.value || 0,
-      today_cost: CostMetric.find_by(date: today, metric_type: :daily_cost)&.value || 0,
-      today_queries: CostMetric.find_by(date: today, metric_type: :daily_queries)&.value || 0,
-      aurora_acu: CostMetric.find_by(date: today, metric_type: :aurora_acu_avg)&.value || 0,
-      s3_documents: CostMetric.find_by(date: today, metric_type: :s3_documents_count)&.value || 0,
-      s3_size_mb: (s3_size_bytes / 1.megabyte.to_f).round(2),
-      s3_size_gb: (s3_size_bytes / 1.gigabyte.to_f).round(2)
-    }
-  end
-
-  def monthly_totals
-    {
-      total_tokens: CostMetric.total_for_month(:daily_tokens),
-      total_cost: CostMetric.total_for_month(:daily_cost),
-      total_queries: CostMetric.total_for_month(:daily_queries),
-      avg_acu: CostMetric.avg_for_month(:aurora_acu_avg).round(2)
-    }
-  end
 
   def last_month_totals
     {
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,35 +1,10 @@
 class HomeController < ApplicationController
+  include MetricsHelper
+
   def index
     @current_metrics = current_metrics
     @monthly_totals = monthly_totals
     @s3_documents_list = S3DocumentsService.new.list_documents
   end
-
-  private
-
-  def current_metrics
-    today = Date.current
-    s3_size_bytes = CostMetric.find_by(date: today, metric_type: :s3_total_size)&.value || 0
-
-    {
-      today_tokens: CostMetric.find_by(date: today, metric_type: :daily_tokens)&.value || 0,
-      today_cost: CostMetric.find_by(date: today, metric_type: :daily_cost)&.value || 0,
-      today_queries: CostMetric.find_by(date: today, metric_type: :daily_queries)&.value || 0,
-      aurora_acu: CostMetric.find_by(date: today, metric_type: :aurora_acu_avg)&.value || 0,
-      s3_documents: CostMetric.find_by(date: today, metric_type: :s3_documents_count)&.value || 0,
-      s3_size_mb: (s3_size_bytes / 1.megabyte.to_f).round(2),
-      s3_size_gb: (s3_size_bytes / 1.gigabyte.to_f).round(2)
-    }
-  end
-
-  def monthly_totals
-    {
-      total_tokens: CostMetric.total_for_month(:daily_tokens),
-      total_cost: CostMetric.total_for_month(:daily_cost),
-      total_queries: CostMetric.total_for_month(:daily_queries),
-      avg_acu: CostMetric.avg_for_month(:aurora_acu_avg).round(2)
-    }
-  end
-
 end
 

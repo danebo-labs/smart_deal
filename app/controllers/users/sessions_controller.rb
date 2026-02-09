d66ffb802fb6336dd6b642cec5ced3d4b4d3c66e
Lahiri San <lahiri.sanchez@gmail.com>
2026-02-09 17:35:10 -0300

    fix redirection on sing in using turbo stream

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,21 +2,9 @@
 
 module Users
   class SessionsController < Devise::SessionsController
-    respond_to :html, :turbo_stream
+    respond_to :html
 
-    def new
-      # If this is a Turbo Stream request, redirect to HTML version
-      # Turbo will handle the redirect automatically
-      if request.format.turbo_stream?
-        redirect_to new_user_session_path(format: :html), status: :see_other
-        return
-      end
-
-      # For HTML requests, use the default behavior
-      super
-    end
-
-    protected
+    private
 
     def auth_options
       { scope: resource_name, recall: "#{controller_path}#new" }

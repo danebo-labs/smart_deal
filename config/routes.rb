60e1e0e1ec497faabd6d1e1112cd975d2ad8c014
Lahiri San <lahiri.sanchez@gmail.com>
2026-02-23 22:02:33 -0300

    removing card to static upload to S3

--- config/routes.rb
+++ config/routes.rb
@@ -19,9 +19,6 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  # Document processing
-  post 'documents/process', to: 'documents#create'
-
   # RAG endpoint for Knowledge Base queries
   post '/rag/ask', to: 'rag#ask'
 

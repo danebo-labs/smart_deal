5038dee30c819fcba4a4a8535e1818ef4c692e69
Lahiri San <lahiri.sanchez@gmail.com>
2026-07-09 20:34:24 -0400

    feat(hosts): scope tenant from host and reject cross-account access
    Resolve current_account from request host instead of user record, and
    block login/session when the user's account does not match the host.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,10 +10,23 @@ class ApplicationController < ActionController::Base
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
 
+  before_action :ensure_user_belongs_to_host_account!, if: :user_signed_in?
+
   private
 
+  def host_account
+    @host_account ||= AccountHostResolver.account_for(request.host) ||
+      raise(ActiveRecord::RecordNotFound, "Unknown host: #{request.host}")
+  end
+
   def current_account
-    @current_account ||= current_user&.account ||
-      raise(ActiveRecord::RecordNotFound, "No account for user #{current_user&.id}")
+    @current_account ||= host_account
+  end
+
+  def ensure_user_belongs_to_host_account!
+    return if current_user.account_id == host_account.id
+
+    sign_out(current_user)
+    redirect_to new_user_session_path, alert: t("errors.host_account_mismatch")
   end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,6 +5,20 @@ module Users
     layout "devise"
     respond_to :html
 
+    def create
+      self.resource = warden.authenticate!(auth_options)
+
+      if resource.account_id != host_account.id
+        sign_out(resource)
+        flash[:alert] = t("errors.host_account_mismatch")
+        redirect_to new_user_session_path and return
+      end
+
+      set_flash_message!(:notice, :signed_in) if is_flashing_format?
+      sign_in(resource_name, resource)
+      respond_with resource, location: after_sign_in_path_for(resource)
+    end
+
     def after_sign_in_path_for(_resource)
       WarmBedrockKbJob.perform_later
       root_path
